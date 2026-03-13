c4f018fa104203cbb5459fd1bee9729cd1d5a7ec
infomomtelo-sketch <infomomtelo@gmail.com>
2026-03-12 17:59:51 -0700

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,12 @@
+function calc(){
 
+const price = Number(document.getElementById("price").value)
+const rent = Number(document.getElementById("rent").value)
+const expenses = Number(document.getElementById("expenses").value)
+
+const cashflow = rent - expenses
+const roi = (cashflow * 12 / price) * 100
+
+document.getElementById("result").innerText =
+"Monthly Cashflow: $" + cashflow + " | ROI: " + roi.toFixed(2) + "%"
+}
